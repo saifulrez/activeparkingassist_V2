d177931ab903ce852460c77ae1a49b0b75aaef5c
saifulrez <89093211+saifulrez@users.noreply.github.com>
2023-07-03 22:33:35 +0800

    Add files via upload

---
+++ Scripts/App/app.js
@@ -0,0 +1,190 @@
+﻿var app = angular.module("apaApp", []); 
+
+app.controller('apaController', apaController);
+
+apaController.$inject = ['$interval', 'states'];
+
+function apaController($interval, states) {
+
+    var vm = this;
+
+    // Functions
+    vm.startSearching = startSearching;
+    vm.cancelSearch = cancelSearch;
+    vm.continueSearch = continueSearch;
+    vm.startParking = startParking;
+    vm.cancelParking = cancelParking;
+
+    vm.engageBrakes = engageBrakes;
+    vm.accelerate = accelerate
+    vm.turnSteeringWheel = turnSteeringWheel;
+
+    vm.reset = reset;
+    vm.obstacleEvent = obstacleEvent;
+    vm.collisionEvent = collisionEvent;
+    vm.hardwareEvent = hardwareEvent;
+    vm.securityEvent = securityEvent;
+
+    // Variables
+    vm.testing = "NOOOOOO!";
+    vm.state = {}
+    vm.states = states;
+
+    vm.maxSpeed = 10;
+
+    // Activate
+    activate();
+
+    function activate() {
+        vm.state = states.idle;
+        vm.findSpot = undefined;
+    }
+
+    function startSearching() {
+        //Error Checks
+        if(vm.state != states.idle){
+            console.log("State error!");
+            return;
+        }
+
+        vm.state = states.searching;
+
+        vm.searchTime = $interval(findSpot, 3000);
+    }
+
+    function cancelSearch() {
+        //Error Checks
+        if (vm.state != states.searching && vm.state != states.found) {
+            console.log("State error!");
+            return;
+        }
+
+        $interval.cancel(vm.searchTime);
+
+        vm.state = states.idle;
+    }
+
+    function continueSearch() {
+        //Error Checks
+        if (vm.state != states.found) {
+            console.log("State error!");
+            return;
+        }
+
+        vm.state = states.searching;
+
+        vm.searchTime = $interval(findSpot, 3000);
+    }
+
+    function findSpot() {
+        $interval.cancel(vm.searchTime);
+        vm.state = states.found;
+    }
+
+    function startParking() {
+        //Error Checks
+        if (vm.state != states.found) {
+            console.log("State error!");
+            return;
+        }
+
+        vm.state = states.parking;
+
+        vm.parkTime = $interval(parkingSuccess, 3000);
+    }
+
+    function cancelParking() {
+        //Error Checks
+        if (vm.state != states.parking) {
+            console.log("State error!");
+            return;
+        }
+
+        $interval.cancel(vm.parkTime);
+
+        vm.state = states.idle;
+    }
+
+    function parkingSuccess() {
+        $interval.cancel(vm.parkTime);
+        vm.state = states.success;
+    }
+
+
+
+    function engageBrakes() {
+        if (vm.state == states.idle && vm.state.mph > 0) {
+            vm.state.mph--;
+        }
+        else if (vm.state == states.searching) {
+            vm.cancelSearch();
+        }
+        else if(vm.state == states.parking){
+            vm.cancelParking();
+        }
+    }
+
+    function accelerate() {
+        if (vm.state == states.idle && vm.state.mph < vm.maxSpeed) {
+            vm.state.mph++;
+        }
+        else if (vm.state == states.searching && vm.state.mph < 5) {
+            vm.state.mph++;
+        }
+        else if (vm.state == states.parking && vm.state.mph < 5) {
+            vm.state.mph++;
+        }
+    }
+
+    function turnSteeringWheel() {
+        console.log("Hit turn steering wheel button");
+    }
+
+
+
+
+    function reset() {
+        vm.state = states.idle;
+    }
+
+    function obstacleEvent() {
+        if (vm.state == states.searching) {
+            vm.cancelSearch();
+            vm.state = states.obstacle;
+        }
+        else if (vm.state == states.parking) {
+            vm.cancelParking();
+            vm.state = states.obstacle;
+        }
+    }
+
+    function collisionEvent() {
+        if (vm.state == states.searching) {
+            vm.cancelSearch();
+            vm.state = states.collision;
+        }
+        else if (vm.state == states.parking) {
+            vm.cancelParking();
+            vm.state = states.collision;
+        } else {
+            vm.state = states.collision;
+        }
+    }
+
+    function hardwareEvent() {
+        if (vm.state == states.searching) {
+            vm.cancelSearch();
+            vm.state = states.failure;
+        }
+        else if (vm.state == states.parking) {
+            vm.cancelParking();
+            vm.state = states.failure;
+        } else {
+            vm.state = states.failure;
+        }
+    }
+
+    function securityEvent() {
+        console.log("Hit security event");
+    }
+};
---
+++ Scripts/App/states.js
@@ -0,0 +1,307 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('apaApp')
+        .factory('states', states);
+
+    //states.$inject = ['$http'];
+
+    function states() {
+
+        var idle = {
+            name: "Default",
+            description: "The system is awaiting input, and the user can start the system if the vehicle is going under 5 mph. The system is also monitoring for any external events, such as a collision or a failure.",
+            mph: 5,
+            accelerator: {
+                input: "Yes",
+                functional: "Yes",
+                active: "Yes"
+            },
+            brakes: {
+                input: "Yes",
+                functional: "Yes",
+                active: "Yes"
+            },
+            steeringWheel: {
+                input: "Yes",
+                functional: "Yes",
+                active: "Yes"
+            },
+            ultrasonic: {
+                input: "N/A",
+                functional: "Yes",
+                active: "No"
+            },
+            camera: {
+                input: "N/A",
+                functional: "Yes",
+                active: "No"
+            }
+        };
+
+        var searching = {
+            name: "Searching",
+            description: "The system is looking for either a parallel or perpendicular parking spot, and the user can cancel the system through the HMI or by braking. The system continues to monitor for events.",
+            mph: 5,
+            accelerator: {
+                input: "Yes (< 5mph)",
+                functional: "Yes",
+                active: "Yes"
+            },
+            brakes: {
+                input: "Yes (cancels search)",
+                functional: "Yes",
+                active: "Yes"
+            },
+            steeringWheel: {
+                input: "no",
+                functional: "Yes",
+                active: "Yes"
+            },
+            ultrasonic: {
+                input: "N/A",
+                functional: "Yes",
+                active: "Yes"
+            },
+            camera: {
+                input: "N/A",
+                functional: "Yes",
+                active: "No"
+            }
+        };
+
+        var found = {
+            name: "Found",
+            description: "The system has found a suitable parking spot, and notifies the user to verify the selection. The user can deny the selection, and the system will search for another. The user can cancel the system through the HMI or by braking. The system continues to monitor for events.",
+            mph: 0,
+            accelerator: {
+                input: "Yes (cancels search)",
+                functional: "Yes",
+                active: "Yes"
+            },
+            brakes: {
+                input: "No",
+                functional: "Yes",
+                active: "Yes"
+            },
+            steeringWheel: {
+                input: "no",
+                functional: "Yes",
+                active: "No"
+            },
+            ultrasonic: {
+                input: "N/A",
+                functional: "Yes",
+                active: "Yes"
+            },
+            camera: {
+                input: "N/A",
+                functional: "Yes",
+                active: "No"
+            }
+        };
+
+        var noMoreSpots = {
+            name: "No More Spots",
+            description: "The system has run out of suitable parking spots and notifies the user of this event. The system then exits back to the default state. The system continues to monitor for events.",
+            mph: 0,
+            accelerator: {
+                input: "Yes",
+                functional: "Yes",
+                active: "No"
+            },
+            brakes: {
+                input: "Yes",
+                functional: "Yes",
+                active: "Yes"
+            },
+            steeringWheel: {
+                input: "Yes",
+                functional: "Yes",
+                active: "No"
+            },
+            ultrasonic: {
+                input: "N/A",
+                functional: "Yes",
+                active: "No"
+            },
+            camera: {
+                input: "N/A",
+                functional: "Yes",
+                active: "No"
+            }
+        };
+
+        var parking = {
+            name: "Parking",
+            description: "The system has found a suitable parking spot, and the user has selected a spot. The system now takes control of the vehicle. The system displays a camera feed. The user can cancel the system through the HMI or by braking. The system continues to monitor for events.",
+            mph: 2,
+            accelerator: {
+                input: "Yes (< 5 mph)",
+                functional: "Yes",
+                active: "Yes"
+            },
+            brakes: {
+                input: "Yes (cancels parking)",
+                functional: "Yes",
+                active: "No"
+            },
+            steeringWheel: {
+                input: "no",
+                functional: "Yes",
+                active: "Yes"
+            },
+            ultrasonic: {
+                input: "N/A",
+                functional: "Yes",
+                active: "Yes"
+            },
+            camera: {
+                input: "N/A",
+                functional: "Yes",
+                active: "Yes"
+            }
+        };
+
+        var success = {
+            name: "Success",
+            description: "The system has parked with no errors. The user may acknowledge the success or simply turn off the vehicle. The system continues to monitor for events.",
+            mph: 0,
+            accelerator: {
+                input: "Yes",
+                functional: "Yes",
+                active: "No"
+            },
+            brakes: {
+                input: "Yes",
+                functional: "Yes",
+                active: "No"
+            },
+            steeringWheel: {
+                input: "Yes",
+                functional: "Yes",
+                active: "No"
+            },
+            ultrasonic: {
+                input: "N/A",
+                functional: "Yes",
+                active: "No"
+            },
+            camera: {
+                input: "N/A",
+                functional: "Yes",
+                active: "No"
+            }
+        };
+
+        var collision = {
+            name: "Collision Detected",
+            description: "The system detects a collision during parking, brakes, and disables the system.",
+            mph: 0,
+            accelerator: {
+                input: "Yes",
+                functional: "Yes",
+                active: "No"
+            },
+            brakes: {
+                input: "Yes",
+                functional: "Yes",
+                active: "No"
+            },
+            steeringWheel: {
+                input: "Yes",
+                functional: "Yes",
+                active: "No"
+            },
+            ultrasonic: {
+                input: "N/A",
+                functional: "Yes",
+                active: "No"
+            },
+            camera: {
+                input: "N/A",
+                functional: "Yes",
+                active: "No"
+            }
+        };
+
+        var failure = {
+            name: "Failure Detected",
+            description: "The system detects a hardware or software failure, and either logs the event, notifies the user, disables the system, or some combination of the above depending on the severity of the event. The system continues to monitor for events.",
+            mph: 0,
+            accelerator: {
+                input: "Yes",
+                functional: "Yes",
+                active: "No"
+            },
+            brakes: {
+                input: "Yes",
+                functional: "Yes",
+                active: "No"
+            },
+            steeringWheel: {
+                input: "Yes",
+                functional: "Yes",
+                active: "No"
+            },
+            ultrasonic: {
+                input: "N/A",
+                functional: "No",
+                active: "No"
+            },
+            camera: {
+                input: "N/A",
+                functional: "No",
+                active: "No"
+            }
+        };
+
+        var obstacle = {
+            name: "Obstacle Detected",
+            description: "The system detects an obstacle during parking, brakes, and disables the system. The system continues to monitor for events.",
+            mph: 0,
+            accelerator: {
+                input: "Yes",
+                functional: "Yes",
+                active: "No"
+            },
+            brakes: {
+                input: "Yes",
+                functional: "Yes",
+                active: "No"
+            },
+            steeringWheel: {
+                input: "Yes",
+                functional: "Yes",
+                active: "No"
+            },
+            ultrasonic: {
+                input: "N/A",
+                functional: "Yes",
+                active: "No"
+            },
+            camera: {
+                input: "N/A",
+                functional: "Yes",
+                active: "No"
+            }
+        };
+
+        var service = {
+            idle: idle,
+            searching: searching,
+            found: found,
+            noMoreSpots: noMoreSpots,
+            parking: parking,
+            success: success,
+            collision: collision,
+            failure: failure,
+            obstacle: obstacle
+
+        };
+
+        return service;
+
+    }
+})();
